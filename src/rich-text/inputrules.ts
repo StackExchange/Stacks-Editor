b810711f38cc1d8c24c2ad10cb36c8eb0eb6ae75
Ham Vocke <hvocke@stackoverflow.com>
2020-12-09 11:25:55 +0100

    add basic support for bold, code, link input rules

--- src/rich-text/inputrules.ts
+++ src/rich-text/inputrules.ts
@@ -5,7 +5,10 @@ import {
     smartQuotes,
     textblockTypeInputRule,
     wrappingInputRule,
+    InputRule,
 } from "prosemirror-inputrules";
+import { MarkType } from "prosemirror-model";
+import { EditorState } from "prosemirror-state";
 import { richTextSchema as schema } from "../shared/schema";
 
 const blockquoteInputRule = wrappingInputRule(
@@ -29,6 +32,57 @@ const orderedListRule = wrappingInputRule(
     (match, node) => node.childCount + <number>node.attrs.order == +match[1]
 );
 
+const inlineCodeRule = markInputRule(
+    /`(\S(?:|.*?\S))`$/,
+    schema.marks.code,
+    {}
+);
+const boldRule = markInputRule(
+    /\*\*(\S(?:|.*?\S))\*\*$/,
+    schema.marks.strong,
+    {}
+);
+
+const linkRule = markInputRule(
+    /\[(.+)\]\((.+)\)$/,
+    schema.marks.link,
+    (match: RegExpMatchArray) => {
+        return { href: match[2] };
+    }
+);
+
+function markInputRule(
+    regexp: RegExp,
+    markType: MarkType,
+    getAttrs:
+        | { [key: string]: any }
+        | ((p: string[]) => { [key: string]: any } | null | undefined)
+) {
+    return new InputRule(
+        regexp,
+        (
+            state: EditorState,
+            match: RegExpMatchArray,
+            start: number,
+            end: number
+        ) => {
+            const attrs =
+                getAttrs instanceof Function ? getAttrs(match) : getAttrs;
+            const tr = state.tr;
+            if (match[1]) {
+                const textStart = start + match[0].indexOf(match[1]);
+                const textEnd = textStart + match[1].length;
+                if (textEnd < end) tr.delete(textEnd, end);
+                if (textStart > start) tr.delete(start, textStart);
+                end = start + match[1].length;
+            }
+            tr.addMark(start, end, markType.create(attrs));
+            tr.removeStoredMark(markType); // Do not continue with mark.
+            return tr;
+        }
+    );
+}
+
 /**
  * Defines all input rules we're using in our rich-text editor.
  * Input rules are formatting operations that trigger as you type based on regular expressions
@@ -49,5 +103,8 @@ export const richTextInputRules = inputRules({
         codeBlockRule,
         unorderedListRule,
         orderedListRule,
+        inlineCodeRule,
+        boldRule,
+        linkRule,
     ],
 });
