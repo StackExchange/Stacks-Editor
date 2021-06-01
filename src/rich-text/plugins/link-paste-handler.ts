dd844dd9fac677bd3271524cb55ed35c44b5424a
Adam Lear <aalear@users.noreply.github.com>
2021-06-01 16:58:28 -0400

    Treat pasting into inline code as plain text

--- src/rich-text/plugins/link-paste-handler.ts
+++ src/rich-text/plugins/link-paste-handler.ts
@@ -1,7 +1,12 @@
-import { Plugin } from "prosemirror-state";
+import { Plugin, EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { Node, Schema } from "prosemirror-model";
 
+/**
+ * Detects if pasted text is a URL (and nothing else)
+ * @param clipboardData The clipboardData from ClipboardEvent
+ * @returns The pasted text if it matches the URL pattern; null otherwise
+ */
 function getDetectedLink(clipboardData: DataTransfer): string | null {
     const content = clipboardData.getData("text/plain");
     if (!content) {
@@ -20,6 +25,20 @@ function getDetectedLink(clipboardData: DataTransfer): string | null {
     return null;
 }
 
+function isInlineCode(state: EditorState): boolean {
+    const { from, $from, to, empty } = state.selection;
+    const schema = state.schema as Schema;
+    if (!empty) {
+        return state.doc.rangeHasMark(from, to, schema.marks.code);
+    }
+
+    return (
+        schema.marks.code.isInSet(state.storedMarks || $from.marks()) !==
+        undefined
+    );
+}
+
+/** Plugin that detects if a URL is being pasted in and automatically formats it as a link */
 export const linkPasteHandler = new Plugin({
     props: {
         handlePaste(view: EditorView, event: ClipboardEvent) {
@@ -29,17 +48,19 @@ export const linkPasteHandler = new Plugin({
                 return false;
             }
 
-            const schema = view.state.schema as Schema;
-            const linkAttrs = { href: link, markup: "linkify" };
-            const node: Node = schema.text(link, [
-                schema.marks.link.create(linkAttrs),
-            ]);
+            if (isInlineCode(view.state)) {
+                view.dispatch(view.state.tr.insertText(link));
+            } else {
+                const schema = view.state.schema as Schema;
+                const linkAttrs = { href: link, markup: "linkify" };
+                const node: Node = schema.text(link, [
+                    schema.marks.link.create(linkAttrs),
+                ]);
 
-            view.dispatch(view.state.tr.replaceSelectionWith(node, false));
+                view.dispatch(view.state.tr.replaceSelectionWith(node, false));
+            }
 
             return true;
         },
     },
 });
-
-// FIXME: ignore pasting into inline code
