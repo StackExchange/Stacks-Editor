9f924e18e9497ee8c20ac29b9fea2735d44586dd
Adam Lear <aalear@users.noreply.github.com>
2021-06-03 17:26:00 -0400

    reuse shared link validation utility function

--- src/rich-text/plugins/link-paste-handler.ts
+++ src/rich-text/plugins/link-paste-handler.ts
@@ -1,29 +1,7 @@
 import { Plugin, EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { Node, Schema } from "prosemirror-model";
-
-/**
- * Detects if pasted text is a URL (and nothing else)
- * @param clipboardData The clipboardData from ClipboardEvent
- * @returns The pasted text if it matches the URL pattern; null otherwise
- */
-function getDetectedLink(clipboardData: DataTransfer): string | null {
-    const content = clipboardData.getData("text/plain");
-    if (!content) {
-        return null;
-    }
-
-    // We may need to beef this up over time, but it should be a decent starting point for
-    // detecting URL pastes.
-    // Credit goes to Keng over at https://stackoverflow.com/a/163684.
-    const urlRegex = /^(https?|ftp):\/\/[-A-Za-z0-9+&@#/%?=~_|!:,.;]*[-A-Za-z0-9+&@#/%=~_|]$/;
-
-    if (urlRegex.test(content)) {
-        return content;
-    }
-
-    return null;
-}
+import { validateLink } from "../../shared/utils";
 
 function isInlineCode(state: EditorState): boolean {
     const { from, $from, to, empty } = state.selection;
@@ -42,9 +20,9 @@ function isInlineCode(state: EditorState): boolean {
 export const linkPasteHandler = new Plugin({
     props: {
         handlePaste(view: EditorView, event: ClipboardEvent) {
-            const link = getDetectedLink(event.clipboardData);
+            const link = event.clipboardData.getData("text/plain");
 
-            if (!link) {
+            if (!link || !validateLink(link)) {
                 return false;
             }
 
