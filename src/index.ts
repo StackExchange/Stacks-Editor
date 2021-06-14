6c1322f4980bb2970fac96dc403fb748f019492e
b-kelly <b-kelly@users.noreply.github.com>
2021-06-14 17:04:11 -0400

    chore: add a purposeful eslint failure

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,5 @@ export {
     EditorType,
 } from "./stacks-editor/editor";
 export type { ExternalEditorPlugin } from "./shared/external-editor-plugin";
+
+console.log("This should cause an eslint failure");
