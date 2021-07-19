5435e9d2abb684b2e3dde90002eaaddb66eb00e9
b-kelly <b-kelly@users.noreply.github.com>
2021-07-19 15:44:00 -0400

    test(e2e): move all e2e tests out of the e2e folder and into their own files

--- test/rich-text/editor.e2e.test.ts
+++ test/rich-text/editor.e2e.test.ts
@@ -1,64 +1,20 @@
-/// <reference types="jest-playwright-preset" />
-/// <reference types="expect-playwright" />
+import {
+    switchMode,
+    getIsMarkdown,
+    getMenu,
+    clearEditor,
+    hasClass,
+    editorSelector,
+    isElementVisible,
+    elementExists,
+    typeText,
+} from "../e2e-helpers";
 
-const editorToggleStateSelector = ".js-editor-toggle-state";
-const editorModeSwitcherSelector = ".js-editor-mode-switcher";
-const editorSelector = ".js-editor";
-const menuSelector = ".js-editor-menu";
-const imageUploaderSelector = ".js-image-uploader";
-const imagePreviewSelector = ".js-image-preview";
-const addImageButtonSelector = ".js-add-image";
-
-const uploadImageMenuItemSelector = ".js-insert-image-btn";
-const insertLinkMenuItemSelector = ".js-insert-link-btn";
 const boldMenuButtonSelector = ".js-bold-btn";
+const insertLinkMenuItemSelector = ".js-insert-link-btn";
 
 const linkViewTooltipSelector = ".js-link-tooltip";
 const removeLinkSelector = ".js-link-tooltip-remove";
-
-const switchMode = async (switchToMarkdown: boolean) => {
-    if ((await getIsMarkdown()) !== switchToMarkdown) {
-        return await page.click(editorModeSwitcherSelector);
-    }
-};
-
-const getMenu = async () => {
-    return await page.$(menuSelector);
-};
-
-const getIsMarkdown = async () => {
-    return await page.$eval(
-        editorToggleStateSelector,
-        (el: HTMLInputElement) => el.checked
-    );
-};
-
-const isElementVisible = async (selector: string) => {
-    return await page.$eval(selector, (el) => !el.classList.contains("d-none"));
-};
-
-const elementExists = async (selector: string) => {
-    return (await page.$(selector)) !== null;
-};
-
-const isEnabled = async (selector: string) => {
-    return await page.$eval(selector, (el: HTMLInputElement) => !el.disabled);
-};
-
-const hasClass = async (selector: string, cssClass: string) => {
-    return await page.$eval(
-        selector,
-        (el: HTMLElement, cssClass) => el.classList.contains(cssClass),
-        cssClass
-    );
-};
-
-const clearEditor = async () => {
-    return await page.$eval(
-        editorSelector,
-        (editor: HTMLElement) => (editor.innerText = "")
-    );
-};
 
 const getMarkdownContent = async () => {
     const wasMarkdownModeActive = await getIsMarkdown();
@@ -68,17 +24,6 @@ const getMarkdownContent = async () => {
         await switchMode(false);
     }
     return text;
-};
-
-/**
- * Type text into the editor window
- * @param text the text to type
- * @param simulateTyping if `true` this will type characters one by one instead of entering the text all at once. That's slower but sometimes necessary
- */
-const typeText = async (text: string, simulateTyping = false) => {
-    return simulateTyping
-        ? await page.type(editorSelector, text)
-        : await page.fill(editorSelector, text);
 };
 
 const enterTextAsMarkdown = async (text: string) => {
@@ -302,80 +247,4 @@ describe("rich-text mode", () => {
             expect(await getMarkdownContent()).toEqual("a link");
         });
     });
-});
-
-describe("markdown mode", () => {
-    beforeAll(async () => {
-        await switchMode(true);
-    });
-
-    it("should show toggle switch", async () => {
-        const isMarkdown = await getIsMarkdown();
-
-        expect(isMarkdown).toBeTruthy();
-    });
-
-    it("should render menu bar", async () => {
-        const menu = await getMenu();
-        expect(menu).not.toBeNull();
-    });
-
-    it("should not highlight bold menu button after click", async () => {
-        await clearEditor();
-
-        expect(await hasClass(boldMenuButtonSelector, "is-selected")).toBe(
-            false
-        );
-        await page.click(boldMenuButtonSelector);
-
-        expect(await hasClass(boldMenuButtonSelector, "is-selected")).toBe(
-            false
-        );
-    });
-});
-
-describe("inserting images", () => {
-    beforeAll(async () => {
-        await switchMode(false);
-    });
-
-    it("should show image upload on keyboard shortcut", async () => {
-        expect(await isElementVisible(imageUploaderSelector)).toBe(false);
-
-        await page.keyboard.down("Control");
-        await page.press(editorSelector, "g");
-        await page.keyboard.up("Control");
-        expect(await isElementVisible(imageUploaderSelector)).toBe(true);
-
-        await page.click(imageUploaderSelector + " .js-cancel-button");
-        expect(await isElementVisible(imageUploaderSelector)).toBe(false);
-    });
-
-    it("should show upload when clicking menu icon", async () => {
-        expect(await isElementVisible(imageUploaderSelector)).toBe(false);
-
-        await page.click(uploadImageMenuItemSelector);
-        expect(await isElementVisible(imageUploaderSelector)).toBe(true);
-
-        await page.click(imageUploaderSelector + " .js-cancel-button");
-        expect(await isElementVisible(imageUploaderSelector)).toBe(false);
-    });
-    it.todo("should show upload when pasting");
-    it.todo("should show upload when dropping a file");
-
-    it("should show image preview", async () => {
-        await page.click(uploadImageMenuItemSelector);
-        const fileInput = await page.$("input[type=file]");
-
-        expect(await isElementVisible(imagePreviewSelector)).toBe(false);
-        expect(await isEnabled(addImageButtonSelector)).toBe(false);
-
-        await fileInput.setInputFiles("./test/e2e/test-image.png");
-
-        expect(await isElementVisible(imagePreviewSelector)).toBe(true);
-        expect(await isEnabled(addImageButtonSelector)).toBe(true);
-    });
-
-    it.todo("should insert uploaded image into document");
-    it.todo("should show placeholder while uploading");
 });
