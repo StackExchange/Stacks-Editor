e19653de423d507f60372f8aae59420e449f1a42
b-kelly <b-kelly@users.noreply.github.com>
2021-11-05 14:00:58 -0400

    docs: separate docs webworker into its own config so hot reload works again

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -6,64 +6,72 @@ const fs = require("fs");
 module.exports = (env, argv) => {
     // add --mode=production to flip this into a pseudo-production server
     const emulateProdServer = argv.mode === "production";
-    return merge(common(env, argv), {
-        entry: {
-            app: "./site/index.ts",
-            serviceworker: "./site/serviceworker.ts",
-        },
-        mode: emulateProdServer ? "production" : "development",
-        devtool: emulateProdServer ? false : "inline-source-map",
-        module: {
-            rules: [
-                {
-                    test: /\.html$/,
-                    use: [
-                        "html-loader",
-                        {
-                            loader: "liquidjs-loader",
-                            options: {
-                                root: "./site/",
-                            },
-                        },
-                    ],
-                },
-            ],
-        },
-        devServer: {
-            open: true,
-            host:
-                // set the host to 0.0.0.0 by default so we can preview the demo on other devices in the same network
-                // NOTE: 0.0.0.0 doesn't work on Windows machines, so settle for localhost instead
-                emulateProdServer && process.platform !== "win32"
-                    ? "0.0.0.0 "
-                    : "localhost",
-            static: {
-                watch: {
-                    ignored: ["test/**/*", "node_modules/**"],
-                },
+    return [
+        merge(common(env, argv), {
+            entry: {
+                app: "./site/index.ts",
             },
-            compress: emulateProdServer,
-        },
-        plugins: [
-            // create an html page for every item in ./site/views
-            ...fs.readdirSync("./site/views").map(
-                (f) =>
-                    new HtmlWebpackPlugin({
-                        template: "./site/views/" + f,
-                        filename: f,
-                    })
-            ),
-        ],
-        optimization: {
-            splitChunks: {
-                cacheGroups: {
-                    // split highlightjs and languages into its own bundle for testing async language loading
-                    hljs: {
-                        test: /highlight.js/,
-                        chunks: "all",
+            mode: emulateProdServer ? "production" : "development",
+            devtool: emulateProdServer ? false : "inline-source-map",
+            module: {
+                rules: [
+                    {
+                        test: /\.html$/,
+                        use: [
+                            "html-loader",
+                            {
+                                loader: "liquidjs-loader",
+                                options: {
+                                    root: "./site/",
+                                },
+                            },
+                        ],
+                    },
+                ],
+            },
+            devServer: {
+                open: true,
+                host:
+                    // set the host to 0.0.0.0 by default so we can preview the demo on other devices in the same network
+                    // NOTE: 0.0.0.0 doesn't work on Windows machines, so settle for localhost instead
+                    emulateProdServer && process.platform !== "win32"
+                        ? "0.0.0.0 "
+                        : "localhost",
+                hot: true,
+                static: {
+                    watch: {
+                        ignored: ["test/**/*", "node_modules/**"],
+                    },
+                },
+                compress: emulateProdServer,
+            },
+            plugins: [
+                // create an html page for every item in ./site/views
+                ...fs.readdirSync("./site/views").map(
+                    (f) =>
+                        new HtmlWebpackPlugin({
+                            template: "./site/views/" + f,
+                            filename: f,
+                        })
+                ),
+            ],
+            optimization: {
+                splitChunks: {
+                    cacheGroups: {
+                        // split highlightjs and languages into its own bundle for testing async language loading
+                        hljs: {
+                            test: /highlight.js/,
+                            chunks: "all",
+                        },
                     },
                 },
             },
-        },
-    });
+        }),
+        merge(common(env, argv), {
+            target: "webworker",
+            entry: {
+                serviceworker: "./site/serviceworker.ts",
+            },
+        }),
+    ];
 };
