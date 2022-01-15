f800d3848f4cbf9a27a011142cd627767e272a5b
Aaron Shekey <aaron@aaronshekey.com>
2022-01-14 19:23:45 -0600

    fix: add aria-labels to icon-only buttons

--- src/shared/menu.ts
+++ src/shared/menu.ts
@@ -223,6 +223,7 @@ export function makeMenuIcon(
     }
 
     button.title = title;
+    button.ariaLabel = title;
     button.dataset.controller = "s-tooltip";
     button.dataset.sTooltipPlacement = "top";
     button.dataset.key = key;
@@ -374,6 +375,7 @@ export function makeMenuLinkEntry(
     dom.href = href;
     dom.target = "_blank";
     dom.title = title;
+    dom.ariaLabel = title;
     dom.dataset.controller = "s-tooltip";
     dom.dataset.sTooltipPlacement = "top";
 
