12a8e81f14fef567b59238df066fe762a6619751
b-kelly <b-kelly@users.noreply.github.com>
2022-01-20 11:11:16 -0500

    ci(eslint): move jest rules to only apply to non-e2e test files

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,16 +11,23 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:@typescript-eslint/recommended-requiring-type-checking",
         "prettier",
-        "plugin:jest/recommended",
-        "plugin:jest/style",
         "plugin:no-unsanitized/DOM",
     ],
     rules: {
         "no-console": "error",
         "no-alert": "error",
-        "jest/no-disabled-tests": "off",
         "no-process-env": "error",
         // TODO this one is a pain to fix w/ ProseMirror's Class<T extends Schema = any> types
         "@typescript-eslint/no-unsafe-argument": "off",
     },
+    overrides: [
+        {
+            // only enable jest rules in non-e2e test files
+            files: ["**/!(*.e2e).test.ts"],
+            extends: ["plugin:jest/recommended", "plugin:jest/style"],
+            rules: {
+                "jest/no-disabled-tests": "off",
+            },
+        },
+    ],
 };
