bae1d6e25f9f410aecc1eead155a5cd2584aafe2
Dan Cormier <dancormierall@gmail.com>
2022-04-21 17:21:38 -0400

    Add conditions for empty

--- src/rich-text/plugins/placeholder.ts
+++ src/rich-text/plugins/placeholder.ts
@@ -1,14 +1,23 @@
 import { Plugin } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 
+// TODO write a test for this
 // TODO cleanup to match common plugin code style
-export const placeholderPlugin = (text = ""): Plugin => {
+export const placeholderPlugin = (placeholderText = ""): Plugin => {
     const update = (view: EditorView) => {
-        // TODO add empty check that accounts for added blocks like blockquote
-        if (view.state.doc.textContent || text === "") {
-            view.dom.removeAttribute("data-placeholder");
+        const { content, textContent } = view.state.doc;
+        // TODO investigate a more dependable check for empty content
+        // TODO check for image upload placeholder
+        const showPlaceholder =
+            placeholderText &&
+            !textContent &&
+            (content.firstChild.type.name === "paragraph" ||
+                content.firstChild.type.name === "heading") &&
+            content.firstChild.childCount === 0;
+        if (showPlaceholder) {
+            view.dom.setAttribute("data-placeholder", placeholderText);
         } else {
-            view.dom.setAttribute("data-placeholder", text);
+            view.dom.removeAttribute("data-placeholder");
         }
     };
 
