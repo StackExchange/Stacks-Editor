77ff68ef7fec5171cf0882084ec9dec8ca8391f7
Ben Kelly <b-kelly@users.noreply.github.com>
2022-05-04 14:55:12 -0400

    build: update dependencies

--- src/commonmark/editor.ts
+++ src/commonmark/editor.ts
@@ -56,6 +56,7 @@ export class CommonmarkEditor extends BaseView {
                         readonlyPlugin(),
                     ],
                 }),
+                plugins: [],
             }
         );
 
