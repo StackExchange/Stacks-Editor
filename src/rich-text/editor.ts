85c53926186b6226ea447ecba10ed0eb500aa68f
Dan Cormier <dancormierall@gmail.com>
2022-05-19 15:54:58 -0400

    fix: open link on Mod-Click
    fixes #62

--- src/rich-text/editor.ts
+++ src/rich-text/editor.ts
@@ -39,6 +39,7 @@ import { codePasteHandler } from "./plugins/code-paste-handler";
 import { linkPasteHandler } from "./plugins/link-paste-handler";
 import { linkPreviewPlugin, LinkPreviewProvider } from "./plugins/link-preview";
 import { linkTooltipPlugin } from "./plugins/link-editor";
+import { linkModClickHandler } from "./plugins/link-mod-click-handler";
 import { plainTextPasteHandler } from "./plugins/plain-text-paste-handler";
 import { spoilerToggle } from "./plugins/spoiler-toggle";
 import { tables } from "./plugins/tables";
@@ -92,6 +93,7 @@ export class RichTextEditor extends BaseView {
                             this.options.codeblockOverrideLanguage
                         ),
                         linkTooltipPlugin(this.options.parserFeatures),
+                        linkModClickHandler(),
                         richTextImageUpload(
                             this.options.imageUpload,
                             this.options.parserFeatures.validateLink,
---
+++ src/rich-text/plugins/link-mod-click-handler.ts
@@ -0,0 +1,39 @@
+import { Plugin, EditorState } from "prosemirror-state";
+import { EditorView } from "prosemirror-view";
+import { Mark } from "prosemirror-model";
+import { richTextSchema as schema } from "../schema";
+
+/**
+ * Returns the mark at cursor if it is of type `link`
+ * @param state The current editor state
+ */
+function findLinkAtCursor(state: EditorState): Mark {
+    const { $from, empty } = state.selection;
+    return (
+        empty && $from.marks().find((mark) => mark.type === schema.marks.link)
+    );
+}
+
+/** Plugin that opens link in new window/tab on Mod-click */
+export const linkModClickHandler = () =>
+    new Plugin({
+        props: {
+            handleClick(
+                this,
+                view: EditorView,
+                pos: number,
+                event: MouseEvent
+            ) {
+                const selectedLink = findLinkAtCursor(view.state);
+                const modPressed = event.getModifierState(
+                    /Mac/.test(navigator.userAgent) ? "Meta" : "Control"
+                );
+
+                if (selectedLink && modPressed) {
+                    window.open(selectedLink.attrs.href, "_blank");
+                }
+
+                return true;
+            },
+        },
+    });
