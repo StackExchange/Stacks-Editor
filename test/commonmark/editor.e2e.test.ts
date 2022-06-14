9a5e9a697fb415be742161ff1e365533a01e6e6d
Dan Cormier <dancormierall@gmail.com>
2022-06-14 15:37:47 -0400

    add tests for double, triple click
    addresses https://github.com/StackExchange/Stacks-Editor/pull/128#pullrequestreview-994031468

--- test/commonmark/editor.e2e.test.ts
+++ test/commonmark/editor.e2e.test.ts
@@ -3,6 +3,7 @@ import {
     clearEditor,
     getIsMarkdown,
     menuSelector,
+    typeText,
     switchMode,
 } from "../e2e-helpers";
 
@@ -27,6 +28,29 @@ test.describe.serial("markdown mode", () => {
 
     test("should render menu bar", async () => {
         await expect(page.locator(menuSelector)).toBeVisible();
+    });
+
+    test("should select word on double click", async () => {
+        await clearEditor(page);
+        await typeText(page, "paragraph here.");
+        await page.dblclick(".js-editor code");
+        const selectedText = await page.evaluate(() =>
+            window.getSelection().toString()
+        );
+        expect(selectedText).toEqual("paragraph");
+    });
+
+    test("should select line on triple click", async () => {
+        await clearEditor(page);
+        await typeText(
+            page,
+            "# Heading 1\n\n```\nconsole.log(window);\n```\n\n- list item 1\n- list item 2\n\nparagraph here."
+        );
+        await page.click(".js-editor .hljs-section", { clickCount: 3 });
+        const selectedText = await page.evaluate(() =>
+            window.getSelection().toString()
+        );
+        expect(selectedText).toEqual("# Heading 1\n");
     });
 
     test("should not highlight bold menu button after click", async () => {
