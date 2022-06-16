1c18a0b36febdc306aeb4c186d64722ee6f54e5f
Dan Cormier <dancormierall@gmail.com>
2022-06-16 12:21:24 -0400

    fix(commonmark-editor): prevent block change from stripping numbers (#140)
    fixes #69

--- src/commonmark/commands.ts
+++ src/commonmark/commands.ts
@@ -192,11 +192,20 @@ function setBlockType(
 /**
  * Returns any block formatting characters (plus trailing space) at the very start of the passed text
  * @param text The text to check for leading block characters
+ * @internal
  */
-function matchLeadingBlockCharacters(text: string) {
+export function matchLeadingBlockCharacters(text: string) {
     // TODO this might be too aggressive... remove based on a whitelist instead?
-    // TODO HACK assumes all block types are non-letter characters followed by a single space
-    return /^[^a-zA-Z]+\s{1}(?=[a-zA-Z_*[!]|$)/.exec(text)?.[0] || "";
+    // Match ordered list markers; see https://spec.commonmark.org/0.30/#ordered-list-marker
+    let match = /^(\d+)(?:\.|\))\s/.exec(text)?.[0];
+
+    // If text is not an ordered list block, check for other block types
+    if (!match) {
+        // TODO HACK assumes all non-ordered list block types are non-letter characters followed by a single space
+        match = /^[^a-zA-Z0-9]+\s{1}(?=[a-zA-Z0-9_*[!]|$)+/.exec(text)?.[0];
+    }
+
+    return match || "";
 }
 
 /**
