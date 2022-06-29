78d7860fd1bf6b40fd5d436424989419f0e8f6f0
Chris Gray <christophergray15@gmail.com>
2022-06-28 22:45:15 -0300

    fix: moved image popover code to img.onclick event instead of ImageView's selectNode()

--- test/rich-text/editor.e2e.test.ts
+++ test/rich-text/editor.e2e.test.ts
@@ -250,5 +250,32 @@ test.describe.serial("rich-text mode", () => {
                 )
             ).toBe(false);
         });
+
+        test("should not show image popover when navigating with left/right arrow keys", async () => {
+            await enterTextAsMarkdown(
+                page,
+                "![an image](https://localhost/some-image)"
+            );
+
+            // move over the image from the left side
+            await page.keyboard.press("ArrowRight");
+            await page.keyboard.press("ArrowRight");
+
+            expect(
+                await page.$eval(imagePopoverSelector, (el) =>
+                    el.classList.contains("is-visible")
+                )
+            ).toBe(false);
+
+            // move over the image from the right side
+            await page.keyboard.press("ArrowLeft");
+            await page.keyboard.press("ArrowLeft");
+
+            expect(
+                await page.$eval(imagePopoverSelector, (el) =>
+                    el.classList.contains("is-visible")
+                )
+            ).toBe(false);
+        });
     });
 });
