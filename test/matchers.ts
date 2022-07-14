feae786173dca1fe4238694b2dddedf43aa8ba25
KyleMit <kmitofsky@stackoverflow.com>
2022-07-14 13:22:47 -0400

    chore(tests): remove unused test method

--- test/matchers.ts
+++ test/matchers.ts
@@ -157,25 +157,3 @@ export function createBasicNodeTree(input: string): CompareTree {
     }
     return root;
 }
-
-describe("createBasicNodeTree", () => {
-    it("creates nested tree", () => {
-        const input = "doc>blockquote>paragraph>1";
-        const expected = {
-            "type.name": "doc",
-            "content": [
-                {
-                    "type.name": "blockquote",
-                    "content": [
-                        {
-                            "type.name": "paragraph",
-                            "childCount": 1,
-                        },
-                    ],
-                },
-            ],
-        };
-        const result = createBasicNodeTree(input);
-        expect(result).toEqual(expected);
-    });
-});
