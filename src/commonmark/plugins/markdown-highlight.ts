e17d277ccdae04cc7b4370508eb142f018aa4ec1
Ben Kelly <b-kelly@users.noreply.github.com>
2022-07-19 13:31:11 -0400

    update lezer classes to use upstream hljs styles

--- src/commonmark/plugins/markdown-highlight.ts
+++ src/commonmark/plugins/markdown-highlight.ts
@@ -3,54 +3,36 @@ import { styleTags, tags, tagHighlighter } from "@lezer/highlight";
 import { highlightPlugin } from "prosemirror-lezer";
 
 export function markdownHighlightPlugin() {
-    /** TODO taken upstream from {@link @lezer/highlight.classHighlighter}, but with new stuff added */
+    /**
+     * Custom classes for rendered tokens to match the hljs classes;
+     * supports all tokens added by @lezer/highlight along with the ones we add in parser.configure below
+     * @see {@link https://github.com/lezer-parser/markdown/blob/3c5f5dc3b5be08e19c32f0f7fac6f3619a58c911/src/markdown.ts#L1833}
+     * Original class names can be found in {@link @lezer/highlight.classHighlighter}
+     */
     const highlighter = tagHighlighter([
-        { tag: tags.link, class: "tok-link" },
-        { tag: tags.heading, class: "tok-heading" },
-        { tag: tags.emphasis, class: "tok-emphasis" },
-        { tag: tags.strong, class: "tok-strong" },
-        { tag: tags.keyword, class: "tok-keyword" },
-        { tag: tags.atom, class: "tok-atom" },
-        { tag: tags.bool, class: "tok-bool" },
-        { tag: tags.url, class: "tok-url" },
-        { tag: tags.labelName, class: "tok-labelName" },
-        { tag: tags.inserted, class: "tok-inserted" },
-        { tag: tags.deleted, class: "tok-deleted" },
-        { tag: tags.literal, class: "tok-literal" },
-        { tag: tags.string, class: "tok-string" },
-        { tag: tags.number, class: "tok-number" },
-        {
-            tag: [tags.regexp, tags.escape, tags.special(tags.string)],
-            class: "tok-string2",
-        },
-        { tag: tags.variableName, class: "tok-variableName" },
-        {
-            tag: tags.local(tags.variableName),
-            class: "tok-variableName tok-local",
-        },
-        {
-            tag: tags.definition(tags.variableName),
-            class: "tok-variableName tok-definition",
-        },
-        { tag: tags.special(tags.variableName), class: "tok-variableName2" },
-        {
-            tag: tags.definition(tags.propertyName),
-            class: "tok-propertyName tok-definition",
-        },
-        { tag: tags.typeName, class: "tok-typeName" },
-        { tag: tags.namespace, class: "tok-namespace" },
-        { tag: tags.className, class: "tok-className" },
-        { tag: tags.macroName, class: "tok-macroName" },
-        { tag: tags.propertyName, class: "tok-propertyName" },
-        { tag: tags.operator, class: "tok-operator" },
-        { tag: tags.comment, class: "tok-comment" },
-        { tag: tags.meta, class: "tok-meta" },
-        { tag: tags.invalid, class: "tok-invalid" },
-        { tag: tags.punctuation, class: "tok-punctuation" },
-
-        // TODO all below added
-        { tag: tags.quote, class: "tok-quote" },
-        { tag: tags.tagName, class: "tok-tagName" },
+        { tag: tags.quote, class: "hljs-quote" },
+        { tag: tags.contentSeparator, class: "hljs-built_in" },
+        { tag: tags.heading1, class: "hljs-section" },
+        { tag: tags.heading2, class: "hljs-section" },
+        { tag: tags.heading3, class: "hljs-section" },
+        { tag: tags.heading4, class: "hljs-section" },
+        { tag: tags.heading5, class: "hljs-section" },
+        { tag: tags.heading6, class: "hljs-section" },
+        { tag: tags.comment, class: "hljs-comment" },
+        { tag: tags.escape, class: "hljs-literal" },
+        { tag: tags.character, class: "hljs-symbol" },
+        { tag: tags.emphasis, class: "hljs-emphasis" },
+        { tag: tags.strong, class: "hljs-strong" },
+        { tag: tags.link, class: "hljs-string" },
+        { tag: tags.monospace, class: "hljs-code" },
+        { tag: tags.url, class: "hljs-link" },
+        { tag: tags.processingInstruction, class: "hljs-symbol" },
+        { tag: tags.labelName, class: "hljs-string" },
+        { tag: tags.string, class: "hljs-string" },
+        { tag: tags.tagName, class: "hljs-tag" },
+        // no highlighting
+        { tag: tags.content, class: "" },
+        { tag: tags.list, class: "" },
     ]);
 
     return highlightPlugin(
