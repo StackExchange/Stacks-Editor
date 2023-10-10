c72b809a413729760fb55921e601e8cfe375f64f
Giamir Buoncristiani <gbuoncristiani@stackoverflow.com>
2023-10-10 16:28:37 +0200

    refactor(hljs): make hljs import for development environments ESM friendly (#257)

--- src/shared/highlighting/hljs-instance.ts
+++ src/shared/highlighting/hljs-instance.ts
@@ -1,24 +1,15 @@
-import type { HLJSApi } from "highlight.js";
+import hljs, { type HLJSApi } from "highlight.js";
 
-/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 /*
  * NOTE: This file is only for local development / generic bundles
  * The prod/stackoverflow build configs completely remove highlight.js from the bundle,
  * expecting it to be supplied globally as `window.hljs`, already configured and ready to go
  */
 
-/** Attempts to get the optional highlight.js instance */
 export function getHljsInstance(): HLJSApi {
     // @ts-expect-error
-    let hljs: HLJSApi = globalThis.hljs as HLJSApi;
+    const hljsInstance: HLJSApi = (globalThis.hljs as HLJSApi) ?? hljs;
 
-    if (!hljs) {
-        try {
-            hljs = require("highlight.js") as HLJSApi;
-        } catch {
-            // Failed to import optional dependency. Do nothing :)
-        }
-    }
-
-    return hljs && hljs.highlight ? hljs : null;
+    return hljsInstance && hljsInstance.highlight ? hljsInstance : null;
 }
