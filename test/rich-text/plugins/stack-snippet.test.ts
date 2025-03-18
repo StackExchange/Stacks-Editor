7139b6a6c38a9a890cfef996fadd97faff87f742
James Boyden <jboyden@stackoverflow.com>
2025-03-18 13:12:07 +0000

    test: paste tests

--- test/rich-text/plugins/stack-snippet.test.ts
+++ test/rich-text/plugins/stack-snippet.test.ts
@@ -7,15 +7,25 @@ import {
     StackSnippetOptions, validateMetaLines
 } from "../../../src/shared/plugins/stack-snippets/common";
 import {EditorState} from "prosemirror-state";
-import {createState, testRichTextSchema} from "../test-helpers";
+import {
+    cleanupPasteSupport,
+    createState, createView, dispatchPasteEvent,
+    setupPasteSupport,
+    testRichTextSchema
+} from "../test-helpers";
 import {
     configureBegin, validBegin, validCss, validEnd, validHtml,
     validJs,
     validSnippetRenderCases
 } from "./stack-snippet-helpers";
 import {
-    parseSnippetBlockForProsemirror
+    parseSnippetBlockForProsemirror, stackSnippetPasteHandler
 } from "../../../src/shared/plugins/stack-snippets/paste-handler";
+import {EditorView} from "prosemirror-view";
+import {Node as ProseMirrorNode} from "prosemirror-model";
+import {
+    StackSnippetView
+} from "../../../src/shared/plugins/stack-snippets/snippet-view";
 
 describe("Stack-snippet", () => {
     describe("commands", () => {
@@ -351,4 +361,70 @@ describe("Stack-snippet", () => {
             });
         });
     });
+
+    describe("paste functionality", () => {
+        beforeAll(setupPasteSupport);
+        afterAll(cleanupPasteSupport);
+
+        const baseState = EditorState.create({
+            schema: testRichTextSchema,
+            plugins: [stackSnippetPasteHandler],
+        });
+
+        it("should do nothing if currently selecting a code block", () => {
+            let state = baseState;
+            state = state.apply(state.tr.replaceRangeWith(0, state.doc.nodeSize - 2, state.schema.nodes.code_block.createChecked()));
+            const view = createView(state);
+
+            dispatchPasteEvent(view.dom, {
+                "text/plain": validBegin + validJs + validEnd,
+            });
+
+            const insertedNode = view.state.selection.$from.parent;
+
+            //Prove we didn't insert any new top-level nodes
+            expect(view.state.doc.firstChild.eq(view.state.doc.lastChild)).toBe(true);
+            expect(insertedNode.isTextblock).toBe(true);
+            expect(insertedNode.textContent).toBe(validBegin + validJs + validEnd);
+        })
+
+        it.each(validSnippetRenderCases)("should render the pasted code as a stack_snippet node", (markdown: string, langs: string[]) => {
+            const view = new EditorView(document.createElement("div"), {
+                state: baseState,
+                nodeViews: {
+                    stack_snippet: (
+                        node: ProseMirrorNode,
+                        view: EditorView,
+                        getPos: () => number
+                    ) => {
+                        return new StackSnippetView(
+                            node,
+                            view,
+                            getPos,
+                            {
+                                renderer: () => Promise.resolve(null),
+                                openSnippetsModal: () => {}
+                            }
+                        );
+                    },
+                }
+            });
+
+            dispatchPasteEvent(view.dom, {
+                "text/plain": markdown,
+            });
+
+            //We expect a trailing paragraph on paste
+            expect(view.state.doc.lastChild.type.name).toBe("paragraph");
+            expect(view.state.doc.lastChild.textContent).toBe("");
+
+            const insertedNode = view.state.doc.firstChild;
+            expect(insertedNode.type.name).toBe("stack_snippet");
+            expect(insertedNode.childCount).toBe(langs.length);
+            for(const child of insertedNode.children){
+                expect(child.type.name).toBe("stack_snippet_lang")
+                expect(langs).toContain(child.attrs["language"])
+            }
+        })
+    })
 });
