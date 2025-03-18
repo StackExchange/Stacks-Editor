0c2a8e4392eab4e4b20c9f75102f4bf4d22738de
James Boyden <jboyden@stackoverflow.com>
2025-03-18 14:21:13 +0000

    fix: lint

--- test/rich-text/plugins/stack-snippet-helpers.ts
+++ test/rich-text/plugins/stack-snippet-helpers.ts
@@ -1,37 +1,47 @@
-export const validBegin: string =
-    `<!-- begin snippet: js hide: false console: true babel: null babelPresetReact: false babelPresetTS: false -->
+export const validBegin: string = `<!-- begin snippet: js hide: false console: true babel: null babelPresetReact: false babelPresetTS: false -->
 
 `;
 
-export const configureBegin = (hide?: boolean, console?: boolean, babel?: boolean, babelPresetReact?: boolean, babelPresetTs?: boolean): string => {
-    const hideStr = hide ? "true" : hide == false ? "false": "null";
-    const consoleStr = console ? "true" : console == false ? "false": "null";
-    const babelStr = babel ? "true" : babel == false ? "false": "null";
-    const babelPresetReactStr = babelPresetReact ? "true" : babelPresetReact == false ? "false": "null";
-    const babelPresetTsStr = babelPresetTs ? "true" : babelPresetTs == false ? "false": "null";
+export const configureBegin = (
+    hide?: boolean,
+    console?: boolean,
+    babel?: boolean,
+    babelPresetReact?: boolean,
+    babelPresetTs?: boolean
+): string => {
+    const hideStr = hide ? "true" : hide == false ? "false" : "null";
+    const consoleStr = console ? "true" : console == false ? "false" : "null";
+    const babelStr = babel ? "true" : babel == false ? "false" : "null";
+    const babelPresetReactStr = babelPresetReact
+        ? "true"
+        : babelPresetReact == false
+          ? "false"
+          : "null";
+    const babelPresetTsStr = babelPresetTs
+        ? "true"
+        : babelPresetTs == false
+          ? "false"
+          : "null";
 
     return `<!-- begin snippet: js hide: ${hideStr} console: ${consoleStr} babel: ${babelStr} babelPresetReact: ${babelPresetReactStr} babelPresetTS: ${babelPresetTsStr} -->
 
 `;
-}
+};
 
 export const validEnd: string = "<!-- end snippet -->";
-export const validJs: string =
-    `<!-- language: lang-js -->
+export const validJs: string = `<!-- language: lang-js -->
 
     console.log("test");
 
 `;
-export const validCss: string =
-    `<!-- language: lang-css -->
+export const validCss: string = `<!-- language: lang-css -->
 
     .test {
       position: fixed;
     }
 
 `;
-export const validHtml: string =
-    `<!-- language: lang-html -->
+export const validHtml: string = `<!-- language: lang-html -->
 
     <div>test</div>
 
@@ -56,17 +66,51 @@ export const validSnippetRenderCases = [
     [validBegin + validHtml + validJs + validEnd, ["js", "html"]],
     [validBegin + validCss + validHtml + validEnd, ["css", "html"]],
     [validBegin + validHtml + validCss + validEnd, ["css", "html"]],
-    [validBegin + validJs + validCss + validHtml + validEnd, ["js", "css", "html"]],
-    [validBegin + validJs + validHtml + validCss + validEnd, ["js", "css", "html"]],
-    [validBegin + validCss + validJs + validHtml + validEnd, ["js", "css", "html"]],
-    [validBegin + validCss + validHtml + validJs + validEnd, ["js", "css", "html"]],
-    [validBegin + validHtml + validCss + validJs + validEnd, ["js", "css", "html"]],
-    [validBegin + validHtml + validJs + validCss + validEnd, ["js", "css", "html"]],
+    [
+        validBegin + validJs + validCss + validHtml + validEnd,
+        ["js", "css", "html"],
+    ],
+    [
+        validBegin + validJs + validHtml + validCss + validEnd,
+        ["js", "css", "html"],
+    ],
+    [
+        validBegin + validCss + validJs + validHtml + validEnd,
+        ["js", "css", "html"],
+    ],
+    [
+        validBegin + validCss + validHtml + validJs + validEnd,
+        ["js", "css", "html"],
+    ],
+    [
+        validBegin + validHtml + validCss + validJs + validEnd,
+        ["js", "css", "html"],
+    ],
+    [
+        validBegin + validHtml + validJs + validCss + validEnd,
+        ["js", "css", "html"],
+    ],
     // HTML comments can be used within the HTML body - even if they happen to look like snippet headers
     [validBegin + validHtmlWithSuspiciousComment("html") + validEnd, ["html"]],
-    [validBegin + validHtmlWithSuspiciousComment("css") + validCss + validEnd, ["html", "css"]],
-    [validBegin + validHtmlWithSuspiciousComment("js") + validJs + validEnd, ["html", "js"]],
-    [validBegin + validHtmlWithSuspiciousComment("html") + validJs + validCss + validEnd, ["js", "css", "html"]],
+    [
+        validBegin +
+            validHtmlWithSuspiciousComment("css") +
+            validCss +
+            validEnd,
+        ["html", "css"],
+    ],
+    [
+        validBegin + validHtmlWithSuspiciousComment("js") + validJs + validEnd,
+        ["html", "js"],
+    ],
+    [
+        validBegin +
+            validHtmlWithSuspiciousComment("html") +
+            validJs +
+            validCss +
+            validEnd,
+        ["js", "css", "html"],
+    ],
 ];
 
 export const invalidSnippetRenderCases = [
@@ -80,9 +124,13 @@ export const invalidSnippetRenderCases = [
     //Missing begin options
     `<!-- begin snippet: js hide: false -->
 
-` + validJs + validEnd,
+` +
+        validJs +
+        validEnd,
     //Unregistered begin options
     `<!-- begin snippet: js hide: false console: true babel: null babelPresetReact: false babelPresetTS: false test: false -->
 
-` + validJs + validEnd,
+` +
+        validJs +
+        validEnd,
 ];
