f92b8166bb2ce9d69498b274814e9f95fe27426a
James Boyden <jboyden@stackoverflow.com>
2025-03-19 16:51:29 +0000

    test: snippet view and markdown-it parser

--- test/rich-text/plugins/stack-snippet.test.ts
+++ test/rich-text/plugins/stack-snippet.test.ts
@@ -13,11 +13,11 @@ import {
     createState,
     createView,
     dispatchPasteEvent,
-    setupPasteSupport,
+    setupPasteSupport, sleepAsync,
     testRichTextSchema,
 } from "../test-helpers";
 import {
-    configureBegin,
+    configureBegin, invalidSnippetRenderCases,
     validBegin,
     validCss,
     validEnd,
@@ -32,6 +32,10 @@ import {
 import { EditorView } from "prosemirror-view";
 import { Node as ProseMirrorNode } from "prosemirror-model";
 import { StackSnippetView } from "../../../src/shared/plugins/stack-snippets/snippet-view";
+import MarkdownIt from "markdown-it";
+import {
+    stackSnippetPlugin
+} from "../../../src/shared/plugins/stack-snippets/schema";
 
 describe("Stack-snippet", () => {
     describe("commands", () => {
@@ -586,6 +590,148 @@ describe("Stack-snippet", () => {
         );
     });
 
-    describe("stackSnippetPlugin (Markdown-it)", () => {});
-    describe("StackSnippetView", () => {});
+    describe("stackSnippetPlugin (Markdown-it)", () => {
+        const mdit = new MarkdownIt("default", {})
+        mdit.use(stackSnippetPlugin);
+
+        it.each(validSnippetRenderCases)("should return a markdown token stream of stack_snippet tokens", (markdown: string, langs: string[]) =>{
+            const tokens = mdit.parse(markdown, {});
+
+            expect(tokens).toHaveLength(langs.length + 2)
+            expect(tokens[0].type).toBe("stack_snippet_open");
+            expect(tokens[0].attrGet("id")).toBeDefined();
+            expect(tokens[0].attrGet("hide")).toBe("false");
+            expect(tokens[0].attrGet("console")).toBe("true");
+            expect(tokens[0].attrGet("babel")).toBe("null");
+            expect(tokens[0].attrGet("babelPresetReact")).toBe("false");
+            expect(tokens[0].attrGet("babelPresetTS")).toBe("false");
+            expect(tokens[tokens.length - 1].type).toBe("stack_snippet_close");
+            //1-indexed, because we want to start from the second element
+            for(let i = 1; i < langs.length + 1; i++){
+                expect(tokens[i].type).toBe("stack_snippet_lang")
+                expect(langs).toContain(tokens[i].attrGet("language"));
+            }
+        });
+
+        it.each(invalidSnippetRenderCases)("should return a token stream that does not include stack_snippet tokens", (markdown: string) =>{
+            const tokens = mdit.parse(markdown, {});
+            const snippetTokenTypes = ["stack_snippet_open", "stack_snippet_close", "stack_snippet_lang"];
+
+            for(const token of tokens){
+                expect(snippetTokenTypes).not.toContain(token.type);
+            }
+        })
+    });
+
+    describe("StackSnippetView", () => {
+        const langNode =
+            testRichTextSchema.nodes.stack_snippet_lang.createChecked(
+                { language: "js" },
+                testRichTextSchema.text("console.log('test');")
+            );
+        const validSnippet =
+            testRichTextSchema.nodes.stack_snippet.createChecked(
+                {
+                    id: "1234",
+                    babel: "true",
+                    babelPresetReact: "true",
+                    babelPresetTS: "null",
+                    console: "true",
+                    hide: "false",
+                },
+                langNode
+            );
+
+        const buildView = (options?: StackSnippetOptions): EditorView => {
+            const state = EditorState.create({
+                schema: testRichTextSchema,
+                plugins: [stackSnippetPasteHandler],
+            });
+            const view = new EditorView(document.createElement("div"), {
+                state: state,
+                nodeViews: {
+                    stack_snippet: (
+                        node: ProseMirrorNode,
+                        view: EditorView,
+                        getPos: () => number
+                    ) => {
+                        return new StackSnippetView(node, view, getPos, options);
+                    },
+                },
+            });
+            //Render the Snippet, which should be rendered as our StackSnippetView
+            view.dispatch(
+                view.state.tr.replaceRangeWith(
+                    0,
+                    view.state.doc.nodeSize - 2,
+                    validSnippet
+                )
+            );
+
+            return view;
+        }
+
+        it("should render run code button if renderer provided", () =>{
+            const view = buildView({
+                renderer: () => Promise.resolve(null),
+                openSnippetsModal: () => {},
+            });
+            const runCodeButton = view.dom.querySelectorAll(".snippet-ctas > button.s-btn");
+
+            expect(runCodeButton).toHaveLength(1);
+            expect(runCodeButton[0].getAttribute("aria-label")).toBe("Run code snippet");
+            expect(runCodeButton[0].getAttribute("title")).toBe("Run code snippet");
+        });
+
+        it("should not render run code button if no renderer provided", () =>{
+            const view = buildView();
+            const runCodeButton = view.dom.querySelectorAll(".snippet-ctas > button.s-btn");
+
+            expect(runCodeButton).toHaveLength(0);
+        })
+
+        it("should call renderer when button clicked", () =>{
+            let buttonClicked = false;
+            const view = buildView({
+                renderer: () => {
+                    buttonClicked = true;
+                    return Promise.resolve(null)
+                },
+                openSnippetsModal: () => {},
+            });
+            const [runCodeButton] = view.dom.querySelectorAll(".snippet-ctas > button.s-btn");
+
+            (<HTMLButtonElement>runCodeButton).click();
+
+            expect(buttonClicked).toBe(true);
+        });
+
+        it("should render returned content when button clicked", async () =>{
+            const renderDoc = document.implementation.createHTMLDocument("test doc");
+            const testDiv = document.createElement("div");
+            testDiv.textContent = "test!";
+            renderDoc.body.appendChild(testDiv);
+            const view = buildView({
+                renderer: () => {
+                    return Promise.resolve(renderDoc);
+                },
+                openSnippetsModal: () => {},
+            });
+            const [runCodeButton] = view.dom.querySelectorAll(".snippet-ctas > button.s-btn");
+
+            (<HTMLButtonElement>runCodeButton).click();
+            // wait for the promise to resolve (immediately) and check that the async content was pulled in
+            await sleepAsync(0);
+
+            const [iframe] = view.dom.querySelectorAll(".snippet-box-result");
+            //Testing the HTMLIFrameElement is a nightmare, instead we're going to grab it's srcdoc and ensure that it parses back to our document
+            const foundDoc = (<HTMLIFrameElement>iframe).srcdoc;
+            const resultDoc = document.implementation.createHTMLDocument();
+            resultDoc.open();
+            resultDoc.write(foundDoc);
+            resultDoc.close();
+            const [resultDiv] = resultDoc.getElementsByTagName("div");
+            expect(resultDiv.textContent).toBe("test!");
+        });
+    });
 });
