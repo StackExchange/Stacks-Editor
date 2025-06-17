14c1c078e36842bf2e8a643b0a633ff2f4a293a8
tgould-stack <tgould@stackoverflow.com>
2025-06-17 10:18:13 -0600

    update exports

--- plugins/official/index.ts
+++ plugins/official/index.ts
@@ -1,1 +1,3 @@
 export { stackSnippetPlugin } from "./stack-snippets/src/stackSnippetPlugin";
+export { StackSnippetOptions } from "./stack-snippets/src/common";
+export { SnippetMetadata } from "./stack-snippets/src/common";
