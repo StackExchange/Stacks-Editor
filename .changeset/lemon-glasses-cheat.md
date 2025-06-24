1c70c0e0c8c349c9f6f1f9e933af618a0842892a
Troy Gould <tgould@stackoverflow.com>
2025-06-24 12:11:54 -0600

    fix(stack-snippet): expose stack-snippet options and metadata types (#457)
    * update exports
    
    * Update plugins/official/index.ts
    
    Co-authored-by: Giamir Buoncristiani <gbuoncristiani@stackoverflow.com>
    
    * adding changeset
    
    * npm format
    
    ---------
    
    Co-authored-by: Giamir Buoncristiani <gbuoncristiani@stackoverflow.com>

---
+++ .changeset/lemon-glasses-cheat.md
@@ -0,0 +1,5 @@
+---
+"@stackoverflow/stacks-editor": patch
+---
+
+Exporting options used by the StacksSnippets plugin for user.
--- plugins/official/index.ts
+++ plugins/official/index.ts
@@ -1,1 +1,5 @@
 export { stackSnippetPlugin } from "./stack-snippets/src/stackSnippetPlugin";
+export type {
+    StackSnippetOptions,
+    SnippetMetadata,
+} from "./stack-snippets/src/common";
