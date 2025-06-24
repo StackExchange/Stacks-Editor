7b2dba6a836274824a5899bbeaa7a9f9de3c9a4b
tgould-stack <tgould@stackoverflow.com>
2025-06-24 09:44:32 -0600

    npm format

--- plugins/official/index.ts
+++ plugins/official/index.ts
@@ -1,2 +1,5 @@
 export { stackSnippetPlugin } from "./stack-snippets/src/stackSnippetPlugin";
-export type { StackSnippetOptions, SnippetMetadata } from "./stack-snippets/src/common";
+export type {
+    StackSnippetOptions,
+    SnippetMetadata,
+} from "./stack-snippets/src/common";
